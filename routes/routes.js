1bf1ac8328f0e4af46625b6efa93f5943835c30a
Iren Galygina <galigina01@yandex.ru>
2022-11-20 18:28:46 +0300

    fix 1

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const { createUserValidation, loginUserValidation } = require('../middlewares/validations');
 const { createUser, loginUser } = require('../controllers/users');
-const auth = require('../middlewares/auth');
 const userRouter = require('./users');
 const movieRouter = require('./movies');
+const auth = require('../middlewares/auth');
 const NotFoundError = require('../errors/NotFoundError');
 const { pageNotFound } = require('../utils/constants');
 
@@ -11,12 +11,12 @@ router.post('/signup', createUserValidation, createUser);
 
 router.post('/signin', loginUserValidation, loginUser);
 
-router.use(auth);
+router.use('/users', auth, userRouter);
 
-router.use('/users', userRouter);
+router.use('/movies', auth, movieRouter);
 
-router.use('/movies', movieRouter);
-
-router.all('*', (_req, _res, next) => {
+router.all('*', auth, (_req, _res, next) => {
   next(new NotFoundError(pageNotFound));
 });
+
+module.exports = router;
